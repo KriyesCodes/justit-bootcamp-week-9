68cb23eb89c62d1eef81ff191ed4101ea263e30f
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-13 14:18:21 +0000

    Complete task 2

--- sequence.py
+++ sequence.py
@@ -2,7 +2,21 @@
 print("hello world")
 
 # Task 1
-print("Kriyes")
-print("London")
-print("I like to play video games")
-print("two plus two is",(2+2),"minus one that's",(2+2-1),"quick mafs")
+# print("Kriyes")
+# print("London")
+# print("I like to play video games")
+# print("two plus two is",(2+2),"minus one that's",(2+2-1),"quick mafs")
+
+# Variables must be declared before they can be used.
+num1 = 10
+num2 = 20
+num3 = 30
+answer1 = num1 + num1
+answer2 = num3 * num2
+answer3 = answer1 + answer2
+
+name = "Kriyes"
+address = "London"
+interest = "play video games"
+
+print("My name is",name,"\nI live in",address,"\nI like to",interest)
