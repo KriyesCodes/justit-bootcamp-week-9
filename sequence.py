bd51a172fab9519c34c787f5da8949792b5dea71
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-13 14:31:58 +0000

    Complete task 3

--- sequence.py
+++ sequence.py
@@ -1,5 +1,5 @@
 # This is a python comment
-print("hello world")
+# print("hello world")
 
 # Task 1
 # print("Kriyes")
@@ -8,16 +8,27 @@ print("hello world")
 # print("two plus two is",(2+2),"minus one that's",(2+2-1),"quick mafs")
 
 # Variables must be declared before they can be used.
-num1 = 10
-num2 = 20
-num3 = 30
-answer1 = num1 + num1
-answer2 = num3 * num2
-answer3 = answer1 + answer2
+# num1 = 10
+# num2 = 20
+# num3 = 30
+# answer1 = num1 + num1
+# answer2 = num3 * num2
+# answer3 = answer1 + answer2
 
 # Task 2
-name = "Kriyes"
-address = "London"
-interest = "play video games"
+# name = "Kriyes"
+# address = "London"
+# interest = "play video games"
 
-print("My name is",name,"\nI live in",address,"\nI like to",interest)
+# print("My name is",name,"\nI live in",address,"\nI like to",interest)
+
+num1 = int(input("Enter your first number: "))
+# JS number, Python = integer (WHOLE numbers), float (DECIMAL numbers e.g. 4.5)
+
+# Task 3: using input function
+# complete the rest of the code:
+# Use a variable and invoke/call the input function to ask the user for a second number
+# add the two numbers# print out the total of the two numbers
+
+num2 = int(input("Enter your second number: "))
+print("Sum is:",num1+num2)
