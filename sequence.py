133bd45ccabf57e53d94f2277147d84d415b9220
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-13 14:05:37 +0000

    Complete task 1

---
+++ sequence.py
@@ -0,0 +1,8 @@
+# This is a python comment
+print("hello world")
+
+# Task 1
+print("Kriyes")
+print("London")
+print("I like to play video games")
+print("two plus two is ", (2+2)," minus one that's ", (2+2-1), " quick mafs")
