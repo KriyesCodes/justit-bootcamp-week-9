3279d3616bf236fa8ba265cf0268435ee56e90ab
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-13 17:11:58 +0000

    Add bytes data type example

--- casting.py
+++ casting.py
@@ -6,3 +6,18 @@
 # float()
 # # convert to boolean
 # bool()
+
+"""create a list"""
+lst=[20,30,32,254]
+print(type(lst), "\n")#returns list type class
+print(lst, "\n")
+
+"""converting a list into bytes"""
+b=bytes([20,30,32,230,200])
+print(type(b), "\n")#returns list type class
+print(b, "\n")
+
+"""converting a list into bytearray"""
+b1=bytearray(lst)
+print(type(b1),"\n")#returns list type class
+print(b1)
