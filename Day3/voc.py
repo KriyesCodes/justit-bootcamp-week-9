66e2f7dffabd0adff571dd8e911488e64aaf05ec
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-15 14:53:44 +0000

    Update task 4

--- Day3/voc.py
+++ Day3/voc.py
@@ -1,11 +1,14 @@
 word = str(input("Enter your word: "))
 
 vowels = ["a","e","i","o","u"]
+consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
 
 for letter in word:
   if letter in vowels:
     desc = "is a vowel"
-  else:
+  elif letter in consonants:
     desc = "is a consonant"
+  else:
+    desc = "is not a valid letter"
   
   print(f"{letter} {desc}")
