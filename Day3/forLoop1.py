bd6e1c925c9bc68685db6b19bb44b63a02e84590
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-15 14:08:35 +0000

    Complete for loop 1 task 2

--- Day3/forLoop1.py
+++ Day3/forLoop1.py
@@ -15,5 +15,20 @@
 
 
 # Task 1 Create a countdown
-for i in range(10, 0, -1):
-  print(i, "mississippi")
+# for i in range(10, 0, -1):
+#   print(i, "mississippi")
+
+# Task 2
+# Complete the code below to iterate through both lists,
+# add comment to explain your code
+
+highscore = [125, 63, 35, 12]
+for score in highscore:
+  #score variable stores the value at the list index
+  print(score)
+
+usersList = ["Anna", "Paul", "Joe", "Jane", "Anne", "Pauline", "Joan", "Janet"]
+
+for user in usersList:
+  #user variable stores the value at the list index
+  print(user)
