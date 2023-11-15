0931954579ab72129996de10b80ba7729dff495e
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-15 14:29:06 +0000

    Complete for loop 1 task 3

--- Day3/forLoop1.py
+++ Day3/forLoop1.py
@@ -18,17 +18,41 @@
 # for i in range(10, 0, -1):
 #   print(i, "mississippi")
 
+#-----------------------
+
 # Task 2
 # Complete the code below to iterate through both lists,
 # add comment to explain your code
 
-highscore = [125, 63, 35, 12]
-for score in highscore:
-  #score variable stores the value at the list index
-  print(score)
+# highscore = [125, 63, 35, 12]
+# for score in highscore:
+#   #score variable stores the value at the list index
+#   print(score)
 
-usersList = ["Anna", "Paul", "Joe", "Jane", "Anne", "Pauline", "Joan", "Janet"]
+# usersList = ["Anna", "Paul", "Joe", "Jane", "Anne", "Pauline", "Joan", "Janet"]
 
-for user in usersList:
-  #user variable stores the value at the list index
-  print(user)
+# for user in usersList:
+#   #user variable stores the value at the list index
+#   print(user)
+
+
+#-----------------------
+
+# Task 3
+# Debug and fix the multiplication program below 
+# add comment where you fix the bugs
+
+print("Welcome to the table quiz.\n")
+num = int(input("Enter a number: "))
+
+
+for i in range(1, num+1):
+  answer = int(input(f" What is {i} x {num}? "))    
+  print(f"Your answer is {answer} ")
+  correct = i * num
+  if answer == correct:
+    print("Correct")
+  else:
+    print(f"Incorrect, the answer is {correct}")
+
+print("Finished")
