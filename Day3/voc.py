1e33a595e8ab6cf35fe8b0df78c30e11ca017296
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-15 15:12:58 +0000

    Update for loop extension task

--- Day3/voc.py
+++ Day3/voc.py
@@ -4,9 +4,9 @@ vowels = ["a","e","i","o","u"]
 consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
 
 for letter in word:
-  if letter in vowels:
+  if letter.lower() in vowels:
     desc = "is a vowel"
-  elif letter in consonants:
+  elif letter.lower() in consonants:
     desc = "is a consonant"
   else:
     desc = "is not a valid letter"
