0a55550fbd47ef4e3f1c3061eda10e1cc6c191c4
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-16 14:30:57 +0000

    Complete sub3 task 1

---
+++ Day4/sub3.py
@@ -0,0 +1,4 @@
+def username(first_name, last_name, interests):
+  print(f"Your first name is: {first_name}\nYour last name is: {last_name}\nYour interests are: {interests}")
+
+username("kriyes", "mahendra", "NOT doing leetcode problems")
