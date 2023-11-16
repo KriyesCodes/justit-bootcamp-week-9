706009dc0e0e483107a7604b1b881676440777b5
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-16 14:17:08 +0000

    Complete sub2 task 1

---
+++ Day4/sub2.py
@@ -0,0 +1,29 @@
+def addition():
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+  answer = num1 + num2;
+  print(f"The answer to {num1} + {num2} is {answer}")
+
+def subtraction():
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+  answer = num1 - num2;
+  print(f"The answer to {num1} - {num2} is {answer}")
+
+def multiplication():
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+  answer = num1 * num2;
+  print(f"The answer to {num1} * {num2} is {answer}")
+
+def division():
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+  answer = num1 / num2;
+  print(f"The answer to {num1} / {num2} is {answer}")
+
+
+addition()
+subtraction()
+multiplication()
+division()
