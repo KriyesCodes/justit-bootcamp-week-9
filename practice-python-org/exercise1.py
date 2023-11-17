b004f5b4d6702605476b428f815368f1b3a524c3
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-17 15:57:02 +0000

    Complete exercise 1 from practicepython.org

---
+++ practice-python-org/exercise1.py
@@ -0,0 +1,7 @@
+currentYear = 2023
+name = str(input("What is your name? "))
+age = int(input("What is your age? "))
+
+yearTurnedHundred = currentYear + (100 - age)
+
+print("Hello",name,"you will turn 100 in the year",yearTurnedHundred)
