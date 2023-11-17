921c4aaa038da0ffb127969c2026a3c3b0b46d31
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-17 14:28:10 +0000

    Complete charDec task

--- Day5/1.charDec.py
+++ Day5/1.charDec.py
@@ -6,28 +6,28 @@
 # ord("a"):This takes a character and returns its decimal equivalent. ​
 
 
-aChar = input("Enter a character: ")
-# ord takes a character and returns its decimal equivalent
-convertChar = ord(aChar)
-print(convertChar)
+# aChar = input("Enter a character: ")
+# # ord takes a character and returns its decimal equivalent
+# convertChar = ord(aChar)
+# print(convertChar)
 
 
-userMsg = "Message"
-number = int(input("Enter number: "))
-convertNum = chr(number)
-secretMsg = userMsg + convertNum
-print(secretMsg)
+# userMsg = "Message"
+# number = int(input("Enter number: "))
+# convertNum = chr(number)
+# secretMsg = userMsg + convertNum
+# print(secretMsg)
 
 
 # chr(97):This takes a decimal number and returns its character equivalent. ​
 
 """Exercise: Complete the code block below to print a list of converted numbers to letter as specified in the range, the 
 questions marks indicates where the code is missing"""
-# def alphabets(): # created the function alphabets
-#     alphabetList = ? # create an empty list
-#     for ? in range(65, 123):
-#         alphabetList.append(chr(letters))  # converts numbers to letters
-#     return ?
+def alphabets(): # created the function alphabets
+    alphabetList = [] # create an empty list
+    for letters in range(65, 123):
+        alphabetList.append(chr(letters))  # converts numbers to letters
+    return alphabetList
 
 
-# print(?)
+print(alphabets())
