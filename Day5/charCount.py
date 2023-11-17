a77b5facc13f00120ef79fde59466ad3422ddb99
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-17 13:43:53 +0000

    Add example files for day 5

---
+++ Day5/charCount.py
@@ -0,0 +1,16 @@
+searchStr = "Python is a great programming language"
+findChar = input("Enter character to search for: ")
+
+"iterate over a string to find an count a character"
+
+# we set count to the value of zero
+
+# loop through the entire string to search for the character entered
+
+    # check if the character entered is a match with any character in the string
+        # increase the count by 1 every time you find the same character
+
+  # display the result
+
+
+# Exercise: refactor the code above by putting it into a subroutine and invoke it
