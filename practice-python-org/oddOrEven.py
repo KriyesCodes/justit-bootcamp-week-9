228a254436cb0f6b509a53b35f82f350cf28550d
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-17 21:49:15 +0000

    Add oddOrEven function

---
+++ practice-python-org/oddOrEven.py
@@ -0,0 +1,10 @@
+def evenOrOdd(num):
+  if (num % 2 == 0):
+    print("The number you input is even")
+  else:
+    print("The number you input is odd")
+
+
+num = int(input("Enter your number: "))
+
+evenOrOdd(num)
