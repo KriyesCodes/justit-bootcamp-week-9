c35f2e1002289c3b5a44ff48cad113d4e22598bd
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-17 21:51:15 +0000

    Add evenOddOrFour function

--- practice-python-org/oddOrEven.py
+++ practice-python-org/oddOrEven.py
@@ -4,7 +4,16 @@ def evenOrOdd(num):
   else:
     print("The number you input is odd")
 
+def evenOddOrFour(num):
+  if (num % 2 == 0):
+    if (num % 4 == 0):
+      print("The number you input is a multiple of 4")
+    else:
+      print("The number you input is even")
+  else:
+    print("The number you input is odd")
+
 
 num = int(input("Enter your number: "))
 
-evenOrOdd(num)
+evenOddOrFour(num)
