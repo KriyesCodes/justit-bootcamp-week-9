e37c6b78b08c4b8fff7ce645d0ab8b4790965eb9
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-25 22:51:23 +0000

    Add removeDuplicatesLoop function to listRemoveDuplicates.py program

--- practice-python-org/listRemoveDuplicates.py
+++ practice-python-org/listRemoveDuplicates.py
@@ -3,4 +3,14 @@ def removeDuplicatesSet(lst):
 
 lst = [1, 2, 3, 3, 5, 10, "ten", "ten", False, False, True]
 
+def removeDuplicatesLoop(lst):
+  newList = []
+
+  for i in range(len(lst)):
+    if lst[i] not in newList:
+      newList.append(lst[i])
+  
+  return newList
+
 print(removeDuplicatesSet(lst))
+print(removeDuplicatesLoop(lst))
