9c44d5d8c0e7dae0b08462d7fe7fb98a66e5dd0e
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-26 14:12:26 +0000

    Add binarySearch function to elementSearch.py program

--- elementSearch.py
+++ elementSearch.py
@@ -4,6 +4,33 @@ def listContainsNumber(sortedList, num):
       return True
   return False
 
+def binarySearch(sortedList, num):
+  leftIndex = 0
+  rightIndex = len(sortedList) - 1
+  # iterations = 0;
+
+  while True:
+    midIndex = (rightIndex + leftIndex) // 2
+    # iterations += 1
+    # print(f"Iteration: {iterations}")
+    # print(f"leftIndex: {leftIndex}")
+    # print(f"rightIndex: {rightIndex}")
+    # print(f"midIndex: {midIndex}")
+    # print("----------------------")
+    
+    if midIndex < leftIndex or midIndex > rightIndex or midIndex < 0:
+      return False
+    
+    if num == sortedList[midIndex]:
+      return True
+    elif num < sortedList[midIndex]:
+      rightIndex = midIndex - 1
+    else:
+      leftIndex = midIndex + 1
+
+    # if iterations > 10:
+    #   break
+
 sortedList1 = [1, 4, 7, 10, 21, 50, 55, 58, 60, 61, 79, 110, 150, 500]
 
 print(listContainsNumber(sortedList1, 500))
